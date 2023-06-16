7c34df8d76c51fcddfec516d13f9bffe81a944df
AnitaChesire <bikamboanita@gmail.com>
2023-06-16 07:44:43 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+julien@ubuntu:~/c/0x00$ cat main.c 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s = 48;
+	int r = 97;
+
+	while ( s <= 57 || n <= 102)
+	{
+		putchar(s);
+		s++;
+	return (0);
+}
