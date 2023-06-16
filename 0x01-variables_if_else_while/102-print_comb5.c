80ba52de6661534fe4c8338ad7b0f56658e55978
AnitaChesire <bikamboanita@gmail.com>
2023-06-16 09:22:35 -0700

    102-print_combo5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,16 +10,16 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 48; x <= 57; x++)
+	for (x = 0; x <= 98; x++)
 	{
-		for (y = x + 1; y <= 57; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
-			putchar(x);
-			putchar(x);
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
 			putchar(' ');
-			putchar(x);
-			putchar(y);
-			if (x == 57 && y == 57)
+			putchar((x / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 98 && y == 99)
 			{
 				continue;
 			}
