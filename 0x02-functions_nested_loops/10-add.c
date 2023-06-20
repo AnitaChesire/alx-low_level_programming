766a60d38f52d309f78ee88bb29049acf0bbf7fb
AnitaChesire <bikamboanita@gmail.com>
2023-06-20 09:42:56 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - adds two numbers
+  * @i: first number
+  * @j: second number
+  * Return: the sum
+*/
+
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
