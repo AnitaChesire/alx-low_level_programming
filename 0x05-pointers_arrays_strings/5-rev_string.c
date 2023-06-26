44cb97f2d40437195fb1f015cdadd8c8ecf3f386
AnitaChesire <bikamboanita@gmail.com>
2023-06-26 14:11:24 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	int i = 0, l = 0;
 	char tmp;
 
-	while(s[i++])
+	while (s[i++])
 		l++;
-	
-	for (i = l -1; i >= l / 2; i--)
+
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		tmp = s[i];
 		s[i] = s[l - i - 1];
