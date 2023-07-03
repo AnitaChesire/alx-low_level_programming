cf82d2589766a5e20207d4f08789c6be812fb3af
AnitaChesire <bikamboanita@gmail.com>
2023-07-03 12:47:48 -0700

    101-crackme_password

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _memcpy - copies
+  * @dest: destination
+  * @src: source
+  * @n: number of bytes
+  * Return: destination
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
