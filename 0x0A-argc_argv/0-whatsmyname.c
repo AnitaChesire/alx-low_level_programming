8a34deff85cc1382f90c39dc06584415bb43d074
AnitaChesire <bikamboanita@gmail.com>
2023-07-07 08:03:46 -0700

    100-change.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - entry point
+  * @argc: argument count
+  * @argv: arguementin array
+  * Return: succes
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc >= 1)
+		printf("%s \n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * main - entry point
+  * @argc: count
+  * @argv: string of arguement
+  * Return: success
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d \n", argc - 1);
+	return (0);
+}
