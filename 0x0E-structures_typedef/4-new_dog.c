302ff3075440f5042206b4006c8ed3dd7618b9b4
AnitaChesire <bikamboanita@gmail.com>
2023-07-17 10:57:30 -0700

    numer 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,35 +6,35 @@
   * @name: name of the dog
   * @age: age of the dog
   * @owner: owner of the dog
-  * Return: nothing
-  */
+  * Return: empty
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	int length_name = 0, length_owner = 0, i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
+	while (name[length_name])
+		length_name++;
+	while (owner[length_owner])
+		length_owner++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	cpyname = malloc(len_name + 1);
+	cpyname = malloc(length_name + 1);
 	if (cpyname == NULL)
 		return (NULL);
 	for (i = 0; name[i]; i++)
 		cpyname[i] = name[i];
 	cpyname[i] = '\0';
 
-	cpyowner = malloc(len_owner + 1);
+	cpyowner = malloc(length_owner + 1);
 	if (cpyowner == NULL)
 		return (NULL);
 	for (i = 0; owner[i]; i++)
