62bc72af6fcdba4c858e5f8b17ce48cbd738d6df
AnitaChesire <bikamboanita@gmail.com>
2023-07-19 10:59:54 -0700

    q3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,12 @@
-#ifndef CALCULATION_H
-#define CALCULATION_H
+#ifndef CALC_H
+#define CALC_H
 
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
 	char *op;
