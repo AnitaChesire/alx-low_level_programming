75cf8a1f2991ff5b2dafc7a15feb37b9bc56b74e
AnitaChesire <bikamboanita@gmail.com>
2023-07-19 11:45:46 -0700

    last last

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -22,9 +22,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	int i = 0;
-	
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
 
-	return (ops[i].f);
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		
+		i++;
+	}
+	
+	return (0);
 }
